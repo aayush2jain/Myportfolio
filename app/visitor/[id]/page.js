f5fcc9da3b0ce40287e92220c3cbc9ccd5419636
aayush2jain <aayushjain1290@gmail.com>
2025-08-31 13:11:15 +0530

    ss

---
+++ app/visitor/[id]/page.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { useEffect,useState } from "react";
+
+export default function VisitorPage({ params }) {
+    const [data, setData] = useState(null);
+    useEffect(()=>{
+       const fetchData = async () => {
+           const response = await axios.get('https://portfolioback-kappa.vercel.app/getvisitor',{params:{id:params.id},withCredentials:true});
+           setData(response.data);
+       };
+       fetchData();
+    }, []);
+   console.log(data);
+
+    return (
+        <div>
+            <h1>Visitor ID: {data}</h1>
+            {/* Render visitor details here */}
+        </div>
+    );
+}
