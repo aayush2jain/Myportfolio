2d4d0d9ff5665d295f3cff9ebb73ad3d8b39c33e
aayush2jain <aayushjain1290@gmail.com>
2025-08-31 18:11:33 +0530

    law

--- app/visitor/[id]/page.js
+++ app/visitor/[id]/page.js
@@ -1,28 +1,31 @@
 "use client";
 import axios from "axios";
-import { useEffect, useState, useMemo } from "react";
+import { useParams } from "next/navigation";
+import { useEffect, useState, useMemo,use } from "react";
 import {
   BarChart, Bar, LineChart, Line,
   XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer
 } from "recharts";
 
-export default function VisitorPage({ params }) {
+export default function VisitorPage() {
   const [data, setData] = useState([]);
-
+  const id = useParams().id;
+  console.log("data", data);
+  console.log("Visitor ID:", id);
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get(
           "https://portfolioback-kappa.vercel.app/getvisitor",
-          { params: { id: params.id }, withCredentials: true }
+          { params: { id }, withCredentials: true }
         );
-        setData(response.data || []);
+        setData(response.data.userDetails || []);
       } catch (err) {
         console.error("Error fetching visitor data:", err);
       }
     };
     fetchData();
-  }, [params.id]);
+  }, [id]);
 
   // ---- Daily Grouping ----
   const dailyData = useMemo(() => {
@@ -67,8 +70,20 @@ export default function VisitorPage({ params }) {
   }, [data]);
 
   return (
+    <>
+    <nav className="fixed w-full top-0 flex justify-center items-center overflow-visible z-50 h-15 bg-[linear-gradient(to_bottom,_black,_#2e2e2e,_#666666)] opacity-60 backdrop-blur-xl">
+        <h1 className="text-3xl font-bold tracking-wider text-white">
+          <a href="/">PORTIGO</a>
+        </h1>
+        <button className="absolute right-0 top-1/2 transform -translate-y-1/2 w-32 h-10 mx-5 bg-indigo-600 text-white text-md rounded-full hover:bg-indigo-700">
+          <a href="/profile">PROFILE</a>
+        </button>
+        {/* <button className="absolute right-40 top-1/2 transform -translate-y-1/2 w-32 h-10 mx-5 bg-indigo-600 text-white text-md rounded-full hover:bg-indigo-700">
+          <a href="/">HOME</a>
+        </button> */}
+      </nav>
     <div className="p-6 grid grid-cols-1 md:grid-cols-2 gap-8">
-      <h1 className="text-2xl font-bold col-span-2">Visitor Analysis (User {params.id})</h1>
+      <h1 className="text-3xl ml-2 mt-10 font-bold col-span-2">Visitor Analysis</h1>
 
       {/* Daily */}
       <div className="shadow-lg rounded-2xl p-4 bg-white">
@@ -115,5 +130,6 @@ export default function VisitorPage({ params }) {
         </ResponsiveContainer>
       </div>
     </div>
+    </>
   );
 }
